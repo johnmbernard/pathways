7333147ffb6e1950757fd5125c78543f7370f77d
Beastnard <john.m.bernard@outlook.com>
2025-11-17 09:25:12 -0500

    Move minimal-app contents to frontend root

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PathwaysDependenciesDemo from "./PathwaysDependenciesDemo";
+
+function App() {
+  return (
+    <div>
+      {/* your existing layout / nav */}
+      <PathwaysDependenciesDemo />
+    </div>
+  );
+}
+
+export default App;
